125007c367f3661e7b9759779348e8973165fdb7
RajeshNathani <rajeshnathani055@gmail.com>
2021-01-21 09:42:23 +0530

    stack

---
+++ stack.py
@@ -0,0 +1,29 @@
+from collections import deque
+
+
+class Stack:
+    def __init__(self):
+        self.container = deque()
+
+    def push(self, val):
+        return self.container.append(val)
+
+    def pop(self):
+        return self.container.pop()
+
+    def is_empty(self):
+        return len(self.container) == 0
+
+    def size_of(self):
+        return len(self.container)
+
+
+s = Stack()
+s.push(5)
+s.push(10)
+s.push(15)
+print(s)
+print(s.size_of())
+s.pop()
+print(s.size_of())
+print(s.is_empty())
